f98c2cc803e490695d26590a667c55acf30288f7
nickland03 <52105217+nickland03@users.noreply.github.com>
2021-07-22 19:38:40 +0300

    reformat

--- AlgoExpert/Easy/sorted_squarred_array.go
+++ AlgoExpert/Easy/sorted_squarred_array.go
@@ -8,7 +8,7 @@ func SortedSquaredArray(array []int) []int {
     arrLength := len(array)
     squaredArray := make([]int, arrLength)
 
-    for i := 0; i < arrLength ; i++ {
+    for i := 0; i < arrLength; i++ {
         squaredNum := array[i] * array[i]
         squaredArray[i] = squaredNum
     }
